167942d0114f4437a8c8595c4cfc9a0ca1bea5e2
Anastasia Polunina <anast.pv@gmail.com>
2018-05-18 16:35:54 +0300

    Naming for Tuple items added.

--- s3backup/Composition/OptionsSource.cs
+++ s3backup/Composition/OptionsSource.cs
@@ -4,17 +4,17 @@ namespace S3Backup.Composition
 {
     public sealed class OptionsSource : IOptionsSource
     {
-        private readonly Lazy<(Options, AmazonOptions, LogOptions)> _lazyOptions;
+        private readonly Lazy<(Options Options, AmazonOptions AmazonOptions, LogOptions LogOptions)> _lazyOptions;
 
         public OptionsSource(string[] args, IArgsParser parser)
         {
             _lazyOptions = new Lazy<(Options, AmazonOptions, LogOptions)>(() => parser.Parse(args));
         }
 
-        public Options Options => _lazyOptions.Value.Item1;
+        public Options Options => _lazyOptions.Value.Options;
 
-        public AmazonOptions AmazonOptions => _lazyOptions.Value.Item2;
+        public AmazonOptions AmazonOptions => _lazyOptions.Value.AmazonOptions;
 
-        public LogOptions LogOptions => _lazyOptions.Value.Item3;
+        public LogOptions LogOptions => _lazyOptions.Value.LogOptions;
     }
 }
